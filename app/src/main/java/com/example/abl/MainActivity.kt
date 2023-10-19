ccaee4d38e1204b2ded892c1d170c7fae54b5362
Derek Christenson <derek.a.christenson@gmail.com>
2023-10-18 22:03:51 -0700

    To bottom p222; works.

--- app/src/main/java/com/example/abl/MainActivity.kt
+++ app/src/main/java/com/example/abl/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.abl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.abl.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.containerFragment) as NavHostFragment
+
+        this.navController = navHostFragment.navController
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        binding.navView.setupWithNavController(this.navController)
+        this.appBarConfiguration =
+            AppBarConfiguration(binding.navView.menu, binding.drawerLayout)
+        setupActionBarWithNavController(this.navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+        (this::navController.isInitialized &&
+                this.navController.navigateUp(this.appBarConfiguration)
+                ) || super.onSupportNavigateUp()
 }
