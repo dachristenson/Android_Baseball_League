81b9f7c2403d79a8b706aebed65785a4fcf27e9e
Derek Christenson <derek.a.christenson@gmail.com>
2024-01-16 22:37:07 -0700

    To bottom p313; works.

--- app/src/main/java/com/example/abl/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/abl/notifications/NotificationsFragment.kt
@@ -72,6 +72,79 @@ class NotificationsFragment : Fragment() {
                     1, //This ID can be used to update/remove an existing notification
                     notification
                 )
+            },
+            NotificationItem(
+                2,
+                "Local - Team",
+                "Displays a local notification for the Pewaukee Princesses, " +
+                        "which when clicked, takes a user to their team page.",
+                NotificationType.Local
+            ) { ctx, _ ->
+                val pendingIntent = NavDeepLinkBuilder(ctx)
+                    .setGraph(R.navigation.nav_graph)
+                    .setDestination(R.id.singleTeamFragment)
+                    .setArguments(Bundle().apply {
+                        putString("teamId", "PKE")
+                        putString("teamName", "Pewaukee Princesses")
+                    }).createPendingIntent()
+
+                val longText = ctx.getString(R.string.pke_notification_long_text)
+
+                val channel = ABLNotificationChannel.Teams
+
+                val notification =
+                    NotificationCompat.Builder(ctx, channel.channelId)
+                        .setContentTitle(ctx.getString(R.string.pke_notification_title))
+                        .setContentText(ctx.getString(R.string.pke_notification_short_text))
+                        .setSmallIcon(R.drawable.fi_ic_crown)
+                        .setStyle(NotificationCompat.BigTextStyle().bigText(longText))
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true)
+                        .build()
+
+                NotificationManagerCompat.from(ctx).notify(
+                    2, //This ID can be used to update/remove an existing notification
+                    notification
+                )
+            },
+
+            NotificationItem(
+                3,
+                "Local - Scoreboard",
+                "Displays a local notification about a score updated, " +
+                        "which when clicked, takes a user to the Scoreboard page.",
+                NotificationType.Local
+            ) { ctx, _ ->
+                val scoreboardPendingIntent = NavDeepLinkBuilder(ctx)
+                    .setGraph(R.navigation.nav_graph)
+                    .setDestination(R.id.scoreboardFragment)
+                    .createPendingIntent()
+
+                val standingsPendingIntent = NavDeepLinkBuilder(ctx)
+                    .setGraph(R.navigation.nav_graph)
+                    .setDestination(R.id.standingsFragment)
+                    .createPendingIntent()
+
+                val channel = ABLNotificationChannel.ScoreUpdates
+
+                val notification =
+                    NotificationCompat.Builder(ctx, channel.channelId)
+                        .setContentTitle(ctx.getString(R.string.scoreboard_notification_title))
+                        .setContentText(ctx.getString(R.string.scoreboard_notification_text))
+                        .setSmallIcon(R.drawable.mdi_scoreboard_outline)
+                        .setContentIntent(scoreboardPendingIntent)
+                        .addAction(
+                            R.drawable.ic_baseline_outlined_flag_24,
+                            ctx.getString(R.string.standings),
+                            standingsPendingIntent
+                        )
+                        .setAutoCancel(true)
+                        .build()
+
+                NotificationManagerCompat.from(ctx).notify(
+                    3, //This ID can be used to update/remove an existing notification
+                    notification
+                )
             }
         )
     }
